7cb8f9b91dc4c857331383b9620421f1adad2820
gearkid1127 <gearkid1127@gmail.com>
2026-05-28 14:02:26 -0500

    styled home page more accurately to the figma design

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -1,26 +1,18 @@
-import { Pressable, Text, View } from 'react-native';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { RootStackParamList } from '../types/navigation';
-import { Blog } from '../types/blog';
-
-
+import { Image, Pressable, Text, View } from "react-native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../types/navigation";
+import { Blog } from "../types/blog";
 
 type Props = {
   blog: Blog;
-  navigation: NativeStackNavigationProp<
-    RootStackParamList,
-    'Home'
-  >;
+  navigation: NativeStackNavigationProp<RootStackParamList, "Home">;
 };
 
-export default function BlogCard({
-  blog,
-  navigation,
-}: Props) {
+export default function BlogCard({ blog, navigation }: Props) {
   return (
     <Pressable
       onPress={() => {
-        navigation.navigate('BlogDetail', {
+        navigation.navigate("BlogDetail", {
           blog,
         });
       }}
@@ -28,10 +20,37 @@ export default function BlogCard({
       <View
         style={{
           padding: 16,
-          borderBottomWidth: 1,
+          flexDirection: "row",
+          gap: 12,
         }}
       >
-        <Text>{blog.title}</Text>
+        <Image
+          source={{ uri: blog.featured_image.url }}
+          style={{
+            width: 150,
+            height: 100,
+            borderRadius: 4,
+          }}
+        />
+
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 18,
+              fontWeight: "600",
+              marginBottom: 8,
+            }}
+          >
+            {blog.title}
+          </Text>
+
+          <Text>{blog.created_at}</Text>
+        </View>
       </View>
     </Pressable>
   );
