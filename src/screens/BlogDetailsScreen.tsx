901015d018fae8704cac54a76b4f3c24c19abc1a
gearkid1127 <gearkid1127@gmail.com>
2026-05-28 10:39:29 -0500

    Set up Expo app structure and navigation

---
+++ src/screens/BlogDetailsScreen.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+
+export default function BlogDetailsScreen() {
+  return (
+    <View
+        style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+        }}
+    >
+        <Text>Blog Details Screen</Text>
+    </View>
+  )
+}
